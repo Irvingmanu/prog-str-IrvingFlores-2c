6251dc1d0625c3f5fe3b20c0bb4281342fe0eb97
Irvingmanu <20253ds070@utez.edu.mx>
2026-01-14 09:43:59 -0600

    agregar operaciones

---
+++ MergeWithOutConflict/src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Semana 2");
+        System.out.println("Versión base");
+
+        System.out.println("2 + 2 = 4");
+        System.out.println("5 * 3 = 15");
+    }
+}
