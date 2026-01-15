8b64e363d5b31eeafc386321ea83a3a7222c79ad
Irvingmanu <20253ds070@utez.edu.mx>
2026-01-14 20:51:58 -0600

    feat: agregar operaciones

--- MergeWithOutConflict/src/mainNew.java
+++ MergeWithOutConflict/src/mainNew.java
@@ -2,5 +2,8 @@ public class mainNew {
     public static void main(String[] args) {
         System.out.println("2 + 2 = 4");
         System.out.println("5 * 3 = 15");
+
+        System.out.println("Semana 2");
+        System.out.println("Versión base");
     }
 }
