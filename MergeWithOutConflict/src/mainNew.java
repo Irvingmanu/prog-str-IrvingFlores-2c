440f49aead7f2035e30b3c6298e0200f9a2f7a43
Irvingmanu <20253ds070@utez.edu.mx>
2026-01-14 22:16:06 -0600

    base conflicto

--- MergeWithOutConflict/src/mainNew.java
+++ MergeWithOutConflict/src/mainNew.java
@@ -1,9 +1,12 @@
 public class mainNew {
     public static void main(String[] args) {
-        System.out.println("2 + 2 = 4");
-        System.out.println("5 * 3 = 15");
 
         System.out.println("Semana 2");
         System.out.println("Versión base");
+
+        System.out.println("2 + 2 = 4");
+        System.out.println("5 * 3 = 15");
+
+
     }
 }
